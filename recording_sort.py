4931305791a234877ea6d4bd40c134fba4a830c4
ericlumsden <elumsden@uw.edu>
2021-07-26 23:33:37 -0700

    recording_sort v1

--- recording_sort.py
+++ recording_sort.py
@@ -9,4 +9,21 @@ recording_sort accomplishes the following:
 
 from create_image import create_image
 import pandas as pd
+import shutil, os
 
+csv_path = input("Enter csv file location: ")
+csv_file = input("Enter csv file name: ")
+df = pandas.read_csv(f'{csv_path}/{csv_file}.csv')
+
+load_count = 0
+for index, row in df.iterrows():
+    print("\r" + f'{load_count}/{len(df)}', end="")
+    os.mkdir(f'{csv_path}/{row['destination']}')
+    files = range(row[first_file], row[last_file])
+
+    for file in files:
+        shutil.copy(f'{row['location']}/{file}.abf', f'{csv_path}/{row['destination']}')
+        create_image(file, row['location'], f'{csv_path}/{row['destination']}')
+    
+    load_count += 1
+    
